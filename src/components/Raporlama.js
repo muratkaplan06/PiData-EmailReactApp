39055737864c7040a374a75542aad65b11b99373
Murat Kaplan <muratkaplan061989@gmail.com>
2024-03-24 21:52:26 +0300

    Rapor sayfası oluşturuldu

--- src/components/Raporlama.js
+++ src/components/Raporlama.js
@@ -1,7 +1,70 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from '@mui/material'
 
 function Raporlama() {
-  return <div>Raporlama</div>
+  const [epostaDetaylar, setEpostaDetaylar] = useState([])
+
+  useEffect(() => {
+    fetchEpostaDetaylar()
+  }, [])
+
+  const fetchEpostaDetaylar = async () => {
+    try {
+      const response = await axios.get(
+        'https://localhost:7012/api/EpostagonderimDetay/EpostagonderimDetaylarWithIncludes',
+      )
+      setEpostaDetaylar(response.data)
+    } catch (error) {
+      console.error('Error fetching eposta detaylar:', error)
+    }
+  }
+
+  return (
+    <div>
+      <h1>E-posta Gönderim Detayları</h1>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Tarih</TableCell>
+              <TableCell>Gönderen E-posta Adresi</TableCell>
+              <TableCell>Konu</TableCell>
+              <TableCell>İçerik</TableCell>
+              <TableCell>İletim Durumu</TableCell>
+              <TableCell>Kişi</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {epostaDetaylar.map((detay) => (
+              <TableRow key={detay.epostaGonderimId}>
+                <TableCell>{detay.gonderimTarihi}</TableCell>
+                <TableCell>
+                  {detay.epostaGonderim.gonderenEpostaAdresi}
+                </TableCell>
+                <TableCell>{detay.epostaGonderim.konusu}</TableCell>
+                <TableCell>{detay.epostaGonderim.icerigi}</TableCell>
+                <TableCell>
+                  {detay.gonderimDurumu ? 'Başarılı' : 'Başarısız'}
+                </TableCell>
+                <TableCell>
+                  {detay.kisi.ad} {detay.kisi.soyad} ({detay.kisi.eposta})
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  )
 }
 
 export default Raporlama
